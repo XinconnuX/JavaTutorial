b164d9f8012962d8ce305d125ea06775bac528e1
XinconnuX <andreluiz_fsa2@hotmail.com>
2021-06-03 20:50:17 -0300

    Commit inicial

---
+++ src/main/java/com/example/javatutorial/JavatutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.javatutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavatutorialApplication.class, args);
+	}
+
+}
